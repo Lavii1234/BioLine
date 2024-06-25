90b0e4f3499030341b206fe7a973219782940737
Lavii1234 <gonzalesvirgil540@gmail.com>
2024-06-25 16:30:31 -0300

    Login com email e senha no firebase

--- assets/js/firebase.js
+++ assets/js/firebase.js
@@ -1,15 +1,18 @@
+// Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-app.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js"
+import { getAuth } from "https://www.gstatic.com/firebasejs/9.6.10/firebase-auth.js";
+//import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDiIVLic_s18atZtBaJmTSNDVjqO_7u1FU",
-  authDomain: "turmaterca-d2878.firebaseapp.com",
-  projectId: "turmaterca-d2878",
-  storageBucket: "turmaterca-d2878.appspot.com",
-  messagingSenderId: "179178679657",
-  appId: "1:179178679657:web:ad5cb958aec320facc21f8",
-  measurementId: "G-492TNV85B3"
+  apiKey: "AIzaSyDXvtN8WsgDjxcOsn5rG9hrHBjLyNpbjDk",
+  authDomain: "tcclala-5e314.firebaseapp.com",
+  projectId: "tcclala-5e314",
+  storageBucket: "tcclala-5e314.appspot.com",
+  messagingSenderId: "133375461156",
+  appId: "1:133375461156:web:26919ea6d6659a084095b1",
+  measurementId: "G-G13Q0GFMJE"
 };
+
 
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app)
